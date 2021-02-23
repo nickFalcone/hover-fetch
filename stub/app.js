a8006d77f5c76c898406985f34341ee52b03b094
nickFalcone <nfalcone@u.rochester.edu>
2021-02-23 11:09:46 -0500

    Set up stub dir for puppeteer testing

---
+++ stub/app.js
@@ -0,0 +1,26 @@
+const hoverFetch = require('../index');
+
+const addLink = document.querySelector('#addLinks');
+const linkList = document.querySelector('.list-of-links');
+
+// Dynamically added links
+addLink.addEventListener('click', () => {
+  const listItem = document.createElement('li');
+  const textNode = document.createTextNode(
+    'a dynamically added link',
+  );
+  const anchor = document.createElement('a');
+  listItem.appendChild(textNode);
+  anchor.appendChild(listItem);
+  anchor.href = 'https://www.example.com/';
+
+  linkList.appendChild(anchor);
+});
+
+// Links at build time
+const anchors = [...document.querySelectorAll('a')];
+
+// Initiate a prefetch request on hover
+anchors.forEach((anchor) => {
+  hoverFetch(anchor);
+});
